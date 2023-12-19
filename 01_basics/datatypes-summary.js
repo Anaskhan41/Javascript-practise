1fca863e9e265f85f30cd5bcd4f93b94d63d286f
Anaskhan41 <150317437+Anaskhan41@users.noreply.github.com>
2023-12-19 14:38:14 +0000

    for interview

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -15,3 +15,27 @@ string => "string"
 object(native and does not implement [(call)]) => "object"
 object(native or host and does implement [(call)]) => "function"
 */
+
+//+++++++++++++++++++++++++++++++++++++++++
+
+// Stack (primitive), Heap (non-primitive)
+
+let myYoutubeName = "xyz"
+
+let anotherName = myYoutubeName
+anotherName = "Jack" 
+
+// console.log(myYoutubeName);
+// console.log(anotherName);
+
+let userOne = {
+    email: "xyz@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "abc@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
