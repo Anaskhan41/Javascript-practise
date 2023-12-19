3409d2867ae02429bb66bde0bf23aca248cc685b
Anaskhan41 <150317437+Anaskhan41@users.noreply.github.com>
2023-12-19 05:53:11 +0000

    conversion in datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,5 +26,69 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **************** Operations ****************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " Anas"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1+2);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(-true);
+// console.log(+"");
+// console.log(+false);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+// gameCounter++;
+++gameCounter;
+// console.log(gameCounter);
+
+// prefix and postix in js
+
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
+
+let x = 3;
+const y = x--;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = --a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
