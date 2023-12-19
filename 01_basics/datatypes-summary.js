86c5b737ef033fdc4d9f8c4367a1774880d72e43
Anaskhan41 <150317437+Anaskhan41@users.noreply.github.com>
2023-12-19 06:56:29 +0000

    for interview

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,17 @@
+// Primitive datatypes
+
+// number, null, boolean, bigInt, string, symbol, undefined
+
+// Reference  (non-Primitive datatypes)
+// Arrays, Object, Functions
+
+// typeof datatypes
+/*
+undefined => "undefined"
+null => "object"
+boolean => "boolean"
+number => "number"
+string => "string"
+object(native and does not implement [(call)]) => "object"
+object(native or host and does implement [(call)]) => "function"
+*/
