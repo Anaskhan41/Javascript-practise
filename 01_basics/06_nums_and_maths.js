acc18a35b2bbc193b54e95b295a06776fdedf16f
Anaskhan41 <150317437+Anaskhan41@users.noreply.github.com>
2023-12-21 16:27:16 +0000

    maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,34 @@
+const score = 400
+// console.log(score);
+
+const balace = new Number(100);
+// console.log(balace);
+
+// console.log(balace.toString());
+// console.log(balace.toString().length);
+// console.log(balace.toFixed(2));
+
+const otherNumber = 128.499
+// console.log(otherNumber.toPrecision(3));
+
+const hundred = 1000000
+// console.log(hundred.toLocaleString());
+// console.log(hundred.toLocaleString('En-IN'));
+
+// +++++++++++++ Maths +++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2)); //upper value
+// console.log(Math.floor(4.9)); 
+// console.log(Math.min(4,6,3,7));
+// console.log(Math.max(4,7,3,6));
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 1;
+const max = 6;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
