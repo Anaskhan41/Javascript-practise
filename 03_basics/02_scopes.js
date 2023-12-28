627250e28d4d24887153dcb3df5285ab9f326b7e
Anaskhan41 <150317437+Anaskhan41@users.noreply.github.com>
2023-12-28 13:03:53 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,42 @@
+//var c = 300
+let a = 300
+if(true){
+    let a = 10
+    const b = 20
+    // var c = 30
+    //console.log("Inner:", a);
+}
+// console.log(a);
+//console.log(b);
+//console.log(c);
+
+function one() {
+    const username = "Anas"
+
+    function two() {
+         const website = "Youtube"
+        //  console.log(username);
+         
+    }
+    //console.log(website);
+    // two()
+}
+
+// one()
+
+if(true) {
+    const username = "Anas"
+    if(username === "Anas") {
+        const website = " Youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// ++++++++++++++++++++ intresting +++++++++++++++++++
+
+function addone(num){
+    return num + 1
+}
