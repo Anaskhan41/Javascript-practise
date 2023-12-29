c1dfaa3632fa0bcae2020b46217fc526af82338d
Anaskhan41 <150317437+Anaskhan41@users.noreply.github.com>
2023-12-29 16:11:51 +0000

    questions

---
+++ 04_control_flow/one.js
@@ -0,0 +1,20 @@
+// if
+
+// if(true){
+//     // if in () the true is there the code will be the execute or if there is false in the () the code will not be execute whic is in {}.
+// }
+// const isuserLoggedIn = true
+// const temperature = 40
+
+// if(temperature < 50) {
+//     console.log("less than 50");
+// } else {
+// console.log("temperature is greater than 50");
+// }
+
+const score = 200
+ 
+if (score > 100) {
+    const power = "fly"
+    console.log(`user power: ${power}`);
+}
