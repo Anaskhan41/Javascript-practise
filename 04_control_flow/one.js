f823618800174359d242ac338c328ebd0cf21470
Anaskhan41 <150317437+Anaskhan41@users.noreply.github.com>
2023-12-30 09:32:25 +0000

    if else

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -12,9 +12,36 @@
 // console.log("temperature is greater than 50");
 // }
 
-const score = 200
+// const score = 200
  
-if (score > 100) {
-    const power = "fly"
-    console.log(`user power: ${power}`);
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+
+const balance = 600
+
+//if(balance > 500) console.log("test"), console.log("test2"); do not code in same line.
+
+// if(balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if(balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggeddInFromGoogle = false
+const loggeddInFromEmail = true
+
+// if (userLoggedIn && debitCard && 2==3) {
+//     console.log("Allow to buy course");
+// }
+
+if (loggeddInFromEmail || loggeddInFromGoogle) {
+    console.log("user logged in");
 }
